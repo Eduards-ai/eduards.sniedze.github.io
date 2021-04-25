25976c4a35fbbea3583493b891887a8cc1f2c029
Eduards Sniedze <eduards.sniedze@gmail.com>
2021-04-25 23:11:00 +0300

    Added Database, Config for prod and dev, user folder for next logic

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { MongooseModule } from "@nestjs/mongoose";
+import { UsersModule } from './users/users.module';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    controllers: [],
+    providers: [],
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: `.${process.env.NODE_ENV}.env`
+        }),
+        MongooseModule.forRoot(`mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PW}@portfolio.ygrx7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`),
+        UsersModule
+    ]
 })
 export class AppModule {}
